080abddcb1c105fa190f09f1be0ea6cb58eb2451
Sergey Linev <S.Linev@gsi.de>
2019-03-21 15:25:01 +0100

    Add comments

--- webapp/model/hListBinding.js
+++ webapp/model/hListBinding.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/base/Log", 
+    "sap/base/Log",
     "sap/ui/model/json/JSONListBinding"
 ], function(Log, JSONListBinding) {
     "use strict";
@@ -20,15 +20,15 @@ sap.ui.define([
            var args = {
               begin: iStartIndex,
               end: iStartIndex + iLength,
-              threshold: iThreshold 
+              threshold: iThreshold
            };
-           
+
            this.getModel().buildFlatNodes(args);
-           
+
            var aNodes = [];
-           
+
            var nodes = this.getModel().getProperty("/nodes");
-           
+
            for (var i = args.begin; i < args.end; i++) {
               var oNode = nodes[i];
               if (oNode) {
@@ -39,11 +39,11 @@ sap.ui.define([
 
                     // QUESTION: seems to be, this is required by JSONListBinding?
                     context: this.getModel().getContext(this.getPath() + "/" + i),
-                    nodeState: {  
-                       expanded: !!oNode._elem._expanded, 
+                    nodeState: {
+                       expanded: !!oNode._elem._expanded,
                        selected: !!oNode._elem._selected,
-                       sum: false
-                    } 
+                       sum: false // ????
+                    }
                  });
               } else {
                  aNodes.push(null); // dummy entry
@@ -51,7 +51,7 @@ sap.ui.define([
            }
 
            Log.warning("root.model.hListBinding#getNodes(" + iStartIndex + ", " + iLength + ", " + iThreshold + ") res = " + aNodes.length);
-           
+
            return aNodes;
         },
 
@@ -63,42 +63,42 @@ sap.ui.define([
         findNode: function() {
             Log.warning("root.model.hListBinding#findNode()");
         },
-    
+
         nodeHasChildren: function(oNode) {
            // Log.warning("root.model.hListBinding#nodeHasChildren(" + oNode.type + ")");
             return oNode.type === "folder";
         },
-    
+
         isExpanded: function(iIndex) {
             var elem = this.getModel().getElementByIndex(iIndex);
             var res = elem ? !!elem._expanded : false;
-            
+
             Log.warning("root.model.hListBinding#isExpanded(" + iIndex + ") res = " + res + "  iselem = " + (elem ? elem._name : "---"));
-            
+
             return res;
         },
 
         expand: function(iIndex) {
             Log.warning("root.model.hListBinding#expand(" + iIndex + ")");
         },
-    
+
         collapse: function(iIndex) {
             Log.warning("root.model.hListBinding#collapse(" + iIndex + ")");
         },
-    
+
         // called by the TreeTable when a node is expanded/collapsed
         toggleIndex: function(iIndex) {
             Log.warning("root.model.hListBinding#toggleIndex(" + iIndex + ")");
             if (this.getModel().toggleNode(iIndex))
                this.checkUpdate(true);
-            
-            // QUESTION: why one should call checkUpdate?, should it be done automatically always? 
+
+            // QUESTION: why one should call checkUpdate?, should it be done automatically always?
         },
-    
+
         getSelectedIndex: function() {
             Log.warning("root.model.hListBinding#getSelectedIndex(" + JSON.stringify(arguments) + ")");
         },
-    
+
         isIndexSelectable: function() {
             Log.warning("root.model.hListBinding#isIndexSelectable(" + JSON.stringify(arguments) + ")");
         }
--- webapp/model/hModel.js
+++ webapp/model/hModel.js
@@ -13,25 +13,25 @@ sap.ui.define([
             this._sBaseUrl = sBaseUrl;
             this.setProperty("/", {
                 nodes: {}, // nodes shown in the TreeTable, should be flat list
-                length: 0  // total number of elements 
+                length: 0  // total number of elements
             });
-            
-            // this is true hierarchy, created on the client side and used for creation of flat list 
+
+            // this is true hierarchy, created on the client side and used for creation of flat list
             this.h = {
                _name: "ROOT",
                _expanded: true
             };
-            
+
             this.loadDataCounter = 0; // counter of number of nodes
-            
+
             this.sortOrder = "";
-            
+
             this.threshold = 100; // default threshold to prefetch items
-            
+
             // submit top-level request already when construct model
             this.submitRequest(this.h, "/");
         },
-        
+
         bindTree: function(sPath, oContext, aFilters, mParameters, aSorters) {
            Log.warning("root.model.hModel#bindTree()");
            this.oBinding = new hListBinding(this, sPath, oContext, aFilters, mParameters, aSorters);
@@ -41,47 +41,47 @@ sap.ui.define([
         getLength: function() {
            return this.getProperty("/length");
         },
-        
+
         getNodeByPath: function(path) {
            var curr = this.h;
            if (!path || (typeof path !== "string") || (path == "/")) return curr;
-           
+
            var names = path.split("/");
-           
+
            while (names.length > 0) {
               var name = names.shift(), find = false;
               if (!name) continue;
-              
+
               for (var k=0;k<curr._childs.length;++k) {
                  if (curr._childs[k]._name == name) {
                     curr = curr._childs[k];
-                    find = true; 
+                    find = true;
                     break;
                  }
               }
-              
+
               if (!find) return null;
            }
            return curr;
         },
 
-        // submit next request to the server 
+        // submit next request to the server
         // now using simple HTTP requests, in ROOT websocket communication will be used
         submitRequest: function(elem, path, first, number) {
-           
+
            if (elem._requested) return;
            elem._requested = true;
-           
+
            this.loadDataCounter++;
-           
+
            var request = "path=" + path;
            if ((first !== undefined) && (number !== undefined))
               request += "&first=" + first + "&number=" + number;
-           
+
            if (this.sortOrder)
               request += "&sort=" + this.sortOrder;
 
-           // TODO: here is just HTTP request, in ROOT we will use websocket to send requests and process replies 
+           // TODO: here is just HTTP request, in ROOT we will use websocket to send requests and process replies
            jQuery.ajax({
               url: this._sBaseUrl + "?" + request,
               dataType: "json",
@@ -89,31 +89,31 @@ sap.ui.define([
           }).done(function(responseData, textStatus, jqXHR) {
 
              this.loadDataCounter--;
-             
+
              this.processResponse(responseData);
-              
+
           }.bind(this)).fail(function(rpath, jqXHR, textStatus, errorThrown) {
-              
+
              this.loadDataCounter--;
-             
+
              // log the error for debugging to see the issue when checking the log
              jQuery.sap.log.error("Failed to read data for " + rpath + "! Reason: " + errorThrown);
-              
+
           }.bind(this, request));
         },
-        
+
         // process reply from server
         // In the future reply will be received via websocket channel
         processResponse: function(reply) {
            var elem = this.getNodeByPath(reply.path);
-           
+
            if (!elem) { console.error('DID NOT FOUND ' + reply.path); return; }
-           
+
            if (!elem._requested) console.error('ELEMENT WAS NOT REQUESTED!!!!', reply.path);
            delete elem._requested;
-           
+
            var smart_merge = false;
-           
+
            if ((elem._nchilds === reply.nchilds) && elem._childs && reply.nodes) {
               if (elem._first + elem._childs.length == reply.first) {
                  elem._childs = elem._childs.concat(reply.nodes);
@@ -130,75 +130,75 @@ sap.ui.define([
               elem._childs = reply.nodes;
               elem._first = reply.first || 0;
            }
-           
+
            this.scanShifts();
 
-           // reset existing nodes if reply does not match with expectation 
+           // reset existing nodes if reply does not match with expectation
            if (!smart_merge)
               this.reset_nodes = true;
 
            if (this.loadDataCounter == 0)
-              if (this.oBinding) 
+              if (this.oBinding)
                  this.oBinding.checkUpdate(true);
         },
-        
-        // return element of hierarchical structure by TreeTable index 
+
+        // return element of hierarchical structure by TreeTable index
         getElementByIndex: function(indx) {
            var nodes = this.getProperty("/nodes"),
                node = nodes ? nodes[indx] : null;
-           
+
            return node ? node._elem : null;
         },
-        
+
         // function used to calculate all ids shifts and total number of elements
         scanShifts: function() {
-           
+
            var id = 0;
-           
+
            function scan(lvl, elem) {
-              
+
               id++;
 
               var before_id = id;
-              
+
               if (elem._expanded) {
                  if (elem._childs === undefined) {
                     // do nothing, childs are not visible as long as we do not have any list
-                    
-                    // id += 0; 
+
+                    // id += 0;
                  } else {
 
                     // gap at the begin
-                    if (elem._first) 
+                    if (elem._first)
                        id += elem._first;
 
                     // jump over all childs
                     for (var k=0;k<elem._childs.length;++k)
                        scan(lvl+1, elem._childs[k]);
-                    
+
                     // gap at the end
                     var _last = (elem._first || 0) + elem._childs.length;
                     var _remains = elem._nchilds  - _last;
                     if (_remains > 0) id += _remains;
                  }
               }
-              
-              // this shift can be later applied to jump over all elements 
-              elem._shift = id - before_id;  
+
+              // this shift can be later applied to jump over all elements
+              elem._shift = id - before_id;
            }
 
            scan(0, this.h);
-           
+
            this.setProperty("/length", id);
-           
+
            return id;
         },
-        
+
         // main  method to create flat list of nodes - only whose which are specified in selection
         // following arguments:
         //    args.begin     - first visisble element from flat list
         //    args.end       - first not-visisble element
-        //    args.threshold - extra elements (before/after) which probably should be prefetched 
+        //    args.threshold - extra elements (before/after) which probably should be prefetched
         // returns total number of nodes
         buildFlatNodes: function(args) {
 
@@ -207,51 +207,51 @@ sap.ui.define([
                threshold = args.threshold || this.threshold || 100,
                threshold2 = Math.round(threshold/2),
                nodes = this.reset_nodes ? {} : this.getProperty("/nodes");
-           
+
            // main method to scan through all existing sub-folders
            function scan(lvl, elem, path) {
               // create elements with safety margin
-              if ((nodes !== null) && !nodes[id] && (id >= args.begin - threshold2) && (id < args.end + threshold2) ) 
+              if ((nodes !== null) && !nodes[id] && (id >= args.begin - threshold2) && (id < args.end + threshold2) )
                  nodes[id] = {
                     name: elem._name,
                     level: lvl,
                     index: id,
                     _elem: elem,
-                    
+
                     // these are optional, should be eliminated in the future
                     type: elem.type,
                     isLeaf: elem.type === "file",
                     expanded: !!elem._expanded
                  };
-              
+
               id++;
 
               if (!elem._expanded) return;
-                 
+
               if (elem._childs === undefined) {
-                 // add new request - can we check if only special part of childs is required? 
+                 // add new request - can we check if only special part of childs is required?
 
                  // TODO: probably one could guess more precise request
                  pthis.submitRequest(elem, path);
-                 
-                 return;  
-              } 
+
+                 return;
+              }
 
               // check if scan is required
               if (((id + elem._shift) < args.begin - threshold2) || (id >= args.end + threshold2)) {
                  id += elem._shift;
                  return;
               }
-                    
+
               // when not all childs from very beginning is loaded, but may be required
               if (elem._first) {
-                       
+
                  // check if requests are needed to load part in the begin of the list
                  if (args.begin - id - threshold2 < elem._first) {
 
                     var first = Math.max(args.begin - id - threshold2, 0),
                         number = Math.min(elem._first - first, threshold);
-                    
+
                     pthis.submitRequest(elem, path, first, number);
                  }
 
@@ -268,24 +268,24 @@ sap.ui.define([
 
               if (_remains > 0) {
                  if (args.end + threshold2 > id) {
-                    
+
                     var first = _last, number = args.end + threshold2 - id;
-                    if (number < threshold) number = threshold; // always request much  
-                    if (number > _remains) number = _remains; // but not too much 
+                    if (number < threshold) number = threshold; // always request much
+                    if (number > _remains) number = _remains; // but not too much
                     if (number > threshold) {
                        first += (number - threshold);
                        number = threshold;
                     }
-                    
+
                     pthis.submitRequest(elem, path, first, number);
                  }
 
                  id += _remains;
-              } 
+              }
            }
-           
+
            scan(0, this.h, "/");
-           
+
            if (this.getProperty("/length") != id) {
               console.error('LENGTH MISMATCH', this.getProperty("/length"), id);
               this.setProperty("/length", id); // update length property
@@ -301,42 +301,42 @@ sap.ui.define([
 
         // toggle expand state of specified node
         toggleNode: function(index) {
-           
+
            var elem = this.getElementByIndex(index);
            if (!elem) return;
-           
+
            console.log('Toggle element', elem._name)
-           
+
            if (elem._expanded) {
               delete elem._expanded;
-              delete elem._childs;
+              delete elem._childs; // TODO: for the future keep childs but make request if expand once again
 
               // close folder - reassign shifts
               this.reset_nodes = true;
               this.scanShifts();
-              
+
               return true;
-              
+
            } else if (elem.type === "folder") {
-              
+
               elem._expanded = true;
               // structure is changing but not immediately
-              
+
               return true;
-              
+
            } else {
               // nothing to do
               return;
            }
-           
+
            // for now - reset all existing nodes and rebuild from the beginning
            // all nodes should be created from scratch
-           
+
            // no need to update - this should be invoked from openui anyway
            //   if (this.oBinding) this.oBinding.checkUpdate(true);
         },
-        
-        
+
+
         // change sorting method, for now server supports default, "direct" and "reverse"
         changeSortOrder: function(newValue) {
            if (newValue === undefined)
@@ -346,22 +346,22 @@ sap.ui.define([
               console.error('WRONG sorting order ', newValue, 'use default');
               newValue = "";
            }
-           
+
            // ignore same value
            if (newValue === this.sortOrder)
               return;
-           
-           
+
+
            this.sortOrder = newValue;
-           
+
            // now we should request values once again
 
            this.submitRequest(this.h, "/");
-           
+
         }
 
     });
-    
+
     return hRootModel;
 
 });
